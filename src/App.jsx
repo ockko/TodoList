0adc02c4111946a8895efc64a933f4c091589d1a
ockko <ocko119@gmail.com>
2024-09-27 23:10:34 +0900

    Data modeling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./comonents/Header";
 import Editor from "./comonents/Editor";
 import List from "./comonents/List";
 
+const mokData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
-  let date = new Date();
+  const [todos, setTodos] = useState(mokData);
 
   return (
     <div className="App">
